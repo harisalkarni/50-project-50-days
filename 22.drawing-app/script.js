7a36c17fe4e343f983e274dc1fa46c14e376cd93
harisalkarni <haris.alkarni@gmail.com>
2023-02-17 17:40:41 +0700

    13 day

---
+++ 22.drawing-app/script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas')
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearEl = document.getElementById('clear')
+
+const ctx = canvas.getContext('2d');
+
+let size = 10
+let isPressed = false
+colorEl.value = 'black'
+let color = colorEl.value
+let x
+let y
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+})
+document.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
+
